100beda1e449b44785e7a5f30e5bec80009141fc
Simon Long <simon@raspberrypi.com>
2026-07-23 09:55:19 +0100

    Use same location for data

--- libfm/data/ui/strip-glade-comments.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# Purge GtkBuilder UI files: strip XML comments and collapse whitespace
-# between tags. Mirrors the .glade.ui rule from the old autotools build.
-set -e
-sed 's/<!--.*-->//' < "$1" | sed ':a;N;$!ba;s/ *\n *</</g' > "$2"
